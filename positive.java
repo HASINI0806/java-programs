f4cf94ace3852bb4390ca836d70583a7d868c406
HASINI0806 <hasini.kolanu06@gmail.com>
2025-09-06 19:25:56 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class positive {
+     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int a=sc.nextInt();
+        if(a>0){
+             System.out.println("the number is +ve");
+        }
+        else{
+             System.out.println("the number is -ve");
+        }
+    }
+    
+}
