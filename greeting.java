b1609edc0e9e78da679d7bfc91a89d58155c9b32
HASINI0806 <hasini.kolanu06@gmail.com>
2025-09-06 19:24:29 +0530

    Add files via upload

---
+++ greeting.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class greeting {
+     public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the username:");
+        String name=sc.next();
+        System.out.println("enter the age:");
+        int age=sc.nextInt();
+        System.out.println("hi..! long time no see ");
+        
+    }
+}
